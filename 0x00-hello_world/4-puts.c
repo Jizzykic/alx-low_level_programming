0d255e9c0a28102af338f9f2b697bc78790c176e
Jizzykic <jizzykic@gmail.com>
2022-09-08 09:26:23 -0700

    task 4-C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,13 @@
 #include<stdio.h>
 
 /**
- 
-   * main - Entry point
- 
     * description: code to print a program
- 
-     * Return: Always 0 (Success)
-      */
+ */
 
 int main(void)
 
 {
-
-		puts("\"Programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
