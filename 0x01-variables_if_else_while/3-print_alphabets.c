3541c5a9869097badfbdd56cd1bd27049ddc2f04
Jizzykic <jizzykic@gmail.com>
2022-09-09 11:52:16 -0700

    task 3-V

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
- * main - Prints the alphabet.
+ * main - Prints the alphabets "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
  *
  * Return: Always 0 (Success)
  */
